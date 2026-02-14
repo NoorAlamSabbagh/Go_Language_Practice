7ee0aed2492a61364c184aa1e0bf94f3da2b8c04
NoorAlamSabbagh <alamrangrej787@gmail.com>
2026-02-15 00:09:38 +0530

    mutex

--- 21_channels/channels.go
+++ 21_channels/channels.go
@@ -5,37 +5,158 @@ import (
 	"time"
 )
 
-// send
+// Channel is used for communication between goroutines.
+
+// func main() {
+// 	messageChan := make(chan string)
+// 	messageChan <- "ping" //message send //(deadlock block)
+// 	msg := <-messageChan  //message recieve
+// 	fmt.Println(msg)
+// }
+
+//
+// func processNum(numChan chan int) {
+// 	fmt.Println("processing number", <-numChan)
+// }
+// func main() {
+// 	numChan := make(chan int)
+// 	go processNum(numChan)
+// 	numChan <- 5
+// 	time.Sleep(time.Second * 2)
+// }
+
+//
+//Sending ka kaam
 // func processNum(numChan chan int) {
 // 	for num := range numChan {
 // 		fmt.Println("processing number", num)
 // 		time.Sleep(time.Second)
 // 	}
-
+// 	// fmt.Println("processing number", <-numChan)
+// }
+// func main() {
+// 	numChan := make(chan int)
+// 	go processNum(numChan)
+// 	for {
+// 		numChan <- rand.Intn(100)
+// 	}
 // }
 
-// receive
+// receive ka kaam
 // func sum(result chan int, num1 int, num2 int) {
 // 	numResult := num1 + num2
 // 	result <- numResult
 // }
+// func main() {
+// 	result := make(chan int)
+// 	go sum(result, 4, 5)//Blocking
+// 	res := <-result
+// 	fmt.Println(res)
+// }
 
-// goroutine synchronizer
+//
 // func task(done chan bool) {
 // 	defer func() { done <- true }()
+// 	fmt.Println("Processing...")
+// }
+// func main() {
+// 	done := make(chan bool)
+// 	go task(done)
+// 	<-done //block
+// }
 
-// 	fmt.Println("processing...")
+//goroutine synchronizer
+// func task(done chan bool) {
+// 	defer func() { done <- true }()
+// 	fmt.Println("Processing...")
+// }
+// func main() {
+// 	done := make(chan bool)
+// 	go task(done)
+// 	<-done //block
+// }
+
+//
+// func main() {
+// 	emailChan := make(chan string, 100)
+// 	emailChan <- "1@example.com"
+// 	emailChan <- "2@example.com"
+
+// 	fmt.Println(<-emailChan)
+// 	fmt.Println(<-emailChan)
+// }
+
+//
+// func emailSender(emailChan <-chan string, done chan<- bool) {
+// 	defer func() { done <- true }()
+
+// 	for email := range emailChan {
+// 		fmt.Println("sending email to", email)
+// 		time.Sleep(time.Second)
+// 	}
+// }
+// func main() {
+// 	emailChan := make(chan string, 100)
+// 	done := make(chan bool)
+// 	go emailSender(emailChan, done)
+// 	for i := 0; i < 2; i++ {
+// 		emailChan <- fmt.Sprintf("%d@gmail.com", i)
+// 	}
+// 	fmt.Println("done sending")
+// 	<-done
+// }
+
+//
+// func emailSender(emailChan <-chan string, done chan<- bool) {
+// 	defer func() { done <- true }()
+
+// 	for email := range emailChan {
+// 		fmt.Println("sending email to", email)
+// 		time.Sleep(time.Second)
+// 	}
+// }
+// func main() {
+// 	emailChan := make(chan string, 100)
+// 	done := make(chan bool)
+// 	go emailSender(emailChan, done)
+// 	for i := 0; i < 2; i++ {
+// 		emailChan <- fmt.Sprintf("%d@gmail.com", i)
+// 	}
+// 	fmt.Println("done sending")
+// 	// this is important to close channel
+// 	close(emailChan)
+// 	<-done
+// }
+
+//
+// func main() {
+// 	chan1 := make(chan int)
+// 	chan2 := make(chan string)
+
+// 	go func() {
+// 		chan1 <- 10
+// 	}()
+
+// 	go func() {
+// 		chan2 <- "pong"
+// 	}()
+// 	for i := 0; i < 2; i++ {
+// 		select {
+// 		case chan1Val := <-chan1:
+// 			fmt.Println("received data from chan1", chan1Val)
+// 		case chan2Val := <-chan2:
+// 			fmt.Println("received data from chan2", chan2Val)
+// 		}
+// 	}
 // }
 
 func emailSender(emailChan <-chan string, done chan<- bool) {
 	defer func() { done <- true }()
-
 	for email := range emailChan {
 		fmt.Println("sending email to", email)
 		time.Sleep(time.Second)
 	}
 }
-
 func main() {
 	chan1 := make(chan int)
 	chan2 := make(chan string)
@@ -47,7 +168,6 @@ func main() {
 	go func() {
 		chan2 <- "pong"
 	}()
-
 	for i := 0; i < 2; i++ {
 		select {
 		case chan1Val := <-chan1:
@@ -56,51 +176,4 @@ func main() {
 			fmt.Println("received data from chan2", chan2Val)
 		}
 	}
-
-	// emailChan := make(chan string, 100)
-	// done := make(chan bool)
-
-	// go emailSender(emailChan, done)
-
-	// for i := 0; i < 5; i++ {
-	// 	emailChan <- fmt.Sprintf("%d@gmail.com", i)
-	// }
-
-	// fmt.Println("done sending.")
-
-	// this is important
-	// close(emailChan)
-	// <-done
-	// emailChan <- "1@example.com"
-	// emailChan <- "2@example.com"
-
-	// fmt.Println(<-emailChan)
-	// fmt.Println(<-emailChan)
-
-	// done := make(chan bool)
-	// go task(done)
-
-	// <-done // block
-
-	// result := make(chan int)
-	// go sum(result, 4, 5)
-	// res := <-result // blocking
-
-	// fmt.Println(res)
-	// numChan := make(chan int)
-
-	// go processNum(numChan)
-
-	// for {
-	// 	numChan <- rand.Intn(100)
-	// }
-
-	// messageChan := make(chan string)
-
-	// messageChan <- "ping" // blocking
-
-	// msg := <-messageChan
-
-	// fmt.Println(msg)
-
 }
--- 22_mutex/mutex.go
+++ 22_mutex/mutex.go
@@ -5,6 +5,75 @@ import (
 	"sync"
 )
 
+// type post struct {
+// 	views int
+// }
+
+// func (p *post) inc() {
+// 	p.views += 1
+// }
+
+// func main() {
+// 	myPost := post{views: 0}
+// 	myPost.inc()
+// 	myPost.inc()
+// 	fmt.Println(myPost.views)
+// }
+
+//
+// type post struct {
+// 	views int
+// }
+
+// func (p *post) inc() {
+// 	p.views += 1
+// }
+
+// func main() {
+// 	myPost := post{views: 0}
+// 	for i := 0; i < 100; i++ {
+// 		myPost.inc()
+// 	}
+// 	fmt.Println(myPost.views)
+// }
+
+//
+// type post struct {
+// 	views int
+// }
+
+// func (p *post) inc() {
+// 	p.views += 1
+// }
+
+// func main() {
+// 	myPost := post{views: 0}
+// 	for i := 0; i < 100; i++ {
+// 		go myPost.inc()
+// 	}
+// 	fmt.Println(myPost.views)
+// }
+
+//
+// type post struct {
+// 	views int
+// }
+
+// func (p *post) inc(wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	p.views += 1
+// }
+// func main() {
+// 	var wg sync.WaitGroup
+// 	myPost := post{views: 0}
+// 	for i := 0; i < 100; i++ {
+// 		wg.Add(1)
+// 		go myPost.inc(&wg)
+// 	}
+// 	wg.Wait()
+// 	fmt.Println(myPost.views)
+// }
+
 type post struct {
 	views int
 	mu    sync.Mutex
@@ -15,7 +84,6 @@ func (p *post) inc(wg *sync.WaitGroup) {
 		p.mu.Unlock()
 		wg.Done()
 	}()
-
 	p.mu.Lock()
 	p.views += 1
 }
@@ -23,13 +91,10 @@ func (p *post) inc(wg *sync.WaitGroup) {
 func main() {
 	var wg sync.WaitGroup
 	myPost := post{views: 0}
-
 	for i := 0; i < 100; i++ {
 		wg.Add(1)
 		go myPost.inc(&wg)
-
 	}
-
 	wg.Wait()
 	fmt.Println(myPost.views)
 }
