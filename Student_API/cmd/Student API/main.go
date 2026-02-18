57ed4f8f04d8d8690efb0224416423188c315fb6
NoorAlamSabbagh <alamrangrej787@gmail.com>
2026-02-18 17:59:57 +0530

    students.go

--- Student_API/cmd/Student API/main.go
+++ Student_API/cmd/Student API/main.go
@@ -1,15 +1,170 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/codersgyan/students-api/internal/config"
+	student "github.com/codersgyan/students-api/internal/http/handlers"
+	// config "github.com/codersgyan/students-api/internal"
+)
+
+// func main() {
+// 	// fmt.Println("Welcome to go API")
+
+//
+// func main() {
+// 	// fmt.Println("Welcome to go API")
+
+// 	//load config
+// 	cfg := config.MustLoad()
+// 	//database setup
+// 	//setup router
+
+// 	//setup server
+// 	router := http.NewServeMux()
+// 	router.HandleFunc("POST /api/students", func(w http.ResponseWriter, r *http.Request) {
+// 		w.Write([]byte("Welcome to students api"))
+// 	})
+// 	// setup server
+// 	server := http.Server{
+// 		Addr:    cfg.Addr,
+// 		Handler: router,
+// 	}
+// 	fmt.Println("Server Started")
+// 	err := server.ListenAndServe()
+// 	if err != nil {
+// 		log.Fatal("failed to start server")
+// 	}
+// }
+
+//
+// func main() {
+// 	// fmt.Println("Welcome to go API")
+
+// 	//load config
+// 	cfg := config.MustLoad()
+// 	//database setup
+// 	//setup router
+
+// 	//setup server
+// 	router := http.NewServeMux()
+// 	router.HandleFunc("POST /api/students", func(w http.ResponseWriter, r *http.Request) {
+// 		w.Write([]byte("Welcome to students api"))
+// 	})
+// 	// setup server
+// 	server := http.Server{
+// 		Addr:    cfg.Addr,
+// 		Handler: router,
+// 	}
+// 	fmt.Println("Server Started %s", cfg.HTTPServer.Addr)
+// 	err := server.ListenAndServe()
+// 	if err != nil {
+// 		log.Fatal("failed to start server")
+// 	}
+// }
+
+//
+// func main() {
+// 	//load config
+// 	cfg := config.MustLoad()
+// 	//database setup
+// 	//setup server
+// 	router := http.NewServeMux()
+// 	router.HandleFunc("POST /api/students", func(w http.ResponseWriter, r *http.Request) {
+// 		w.Write([]byte("Welcome to students api"))
+// 	})
+// 	// setup server
+// 	server := http.Server{
+// 		Addr:    cfg.Addr,
+// 		Handler: router,
+// 	}
+// 	slog.Info("Server Started", slog.String("address", cfg.Addr))
+// 	done := make(chan os.Signal, 1)
+// 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+// 	go func() {
+// 		err := server.ListenAndServe()
+// 		if err != nil {
+// 			log.Fatal("failed to start server")
+// 		}
+// 	}()
+// 	<-done
+// 	slog.Info("Shutting down the server")
+// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Millisecond)
+// 	defer cancel()
+// 	if err := server.Shutdown(ctx); err != nil {
+// 		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+// 	}
+// 	slog.Info("shutting down the server")
+// }
+
+// Now importing student routes
 func main() {
-	fmt.Println("Welcome to go API")
-
+	//load config
+	cfg := config.MustLoad()
+	//database setup
+	//setup server
+	router := http.NewServeMux()
+	router.HandleFunc("POST /api/students", student.New())
+	// setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+	slog.Info("Server Started", slog.String("address", cfg.Addr))
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start server")
+		}
+	}()
+	<-done
+	slog.Info("Shutting down the server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Millisecond)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	}
+	slog.Info("shutting down the server")
 }
 
 //
-// package main
 
+// 	//load config
+// 	cfg := config.MustLoad()
+// 	//database setup
+// 	//setup router
+
+// 	//setup server
+// 	router := http.NewServeMux()
+// 	router.HandleFunc("POST /api/students", func(w http.ResponseWriter, r *http.Request) {
+// 		w.Write([]byte("Welcome to students api"))
+// 	})
+// 	// setup server
+// 	server := http.Server{
+// 		Addr:    cfg.Addr,
+// 		Handler: router,
+// 	}
+// 	fmt.Println("Server Started")
+// 	err := server.ListenAndServe()
+// 	if err != nil {
+// 		log.Fatal("failed to start server")
+// 	}
+// }
+
+// router.HandleFunc("POST /api/students", student.New(storage))
+// 	router.HandleFunc("GET /api/students/{id}", student.GetById(storage))
+// 	router.HandleFunc("GET /api/students", student.GetList(storage))
+
+//
 // import (
 // 	"context"
 // 	"log"
@@ -26,8 +181,7 @@ func main() {
 // )
 
 // func main() {
-// 	// load config
-// 	cfg := config.MustLoad()
+// 	// load confi
 // 	// database setup
 
 // 	storage, err := sqlite.New(cfg)
@@ -40,15 +194,7 @@ func main() {
 // 	// setup router
 // 	router := http.NewServeMux()
 
-// 	router.HandleFunc("POST /api/students", student.New(storage))
-// 	router.HandleFunc("GET /api/students/{id}", student.GetById(storage))
-// 	router.HandleFunc("GET /api/students", student.GetList(storage))
-// 	// setup server
-
-// 	server := http.Server{
-// 		Addr:    cfg.Addr,
-// 		Handler: router,
-// 	}
+//
 
 // 	slog.Info("server started", slog.String("address", cfg.Addr))
 
@@ -56,23 +202,12 @@ func main() {
 
 // 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
-// 	go func() {
-// 		err := server.ListenAndServe()
-// 		if err != nil {
-// 			log.Fatal("failed to start server")
-// 		}
-// 	}()
-
 // 	<-done
 
-// 	slog.Info("shutting down the server")
+//
 
 // 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 // 	defer cancel()
-
-// 	if err := server.Shutdown(ctx); err != nil {
-// 		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
-// 	}
 
 // 	slog.Info("server shutdown successfully")
 
